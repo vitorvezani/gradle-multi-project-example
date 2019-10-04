0e00fecbd474f3d1d4a9cc59a8f216d4c753937b
Vitor Rodrigo Vezani <vitor.vezani@freshcells.de>
2019-10-04 15:09:43 +0200

    Add tests

---
+++ template-server/src/test/java/io/jeffchao/template/server/ServerTest.java
@@ -0,0 +1,22 @@
+package io.jeffchao.template.server;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ServerTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+  
+  @Test
+  public void shouldPassThisTest() {
+    new Server();
+  }
+}
